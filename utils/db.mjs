51ac6dbc4b24ab9c65c4d0d30beaca38bd2f227c
Miteto08 <julien.lechevalier@shortways.com>
2025-01-10 14:57:57 +0100

    Task1 - MongoDB utils

--- utils/db.mjs
+++ utils/db.mjs
@@ -8,7 +8,7 @@ const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
     constructor() {
-        MongoClient.connect(url, async (error, client) => {
+        MongoClient.connect(url, (error, client) => {
             if (error) {
                 console.log(error.message);
                 this.db = false;
@@ -17,23 +17,6 @@ class DBClient {
             this.db = client.db(DB_DATABASE);
             this.users = this.db.collection('users');
             this.files = this.db.collection('files');
-
-            const usersCount = await this.users.countDocuments();
-            if (usersCount === 0) {
-                const users = [
-                    { name: 'John' },
-                    { name: 'Jane' },
-                    { name: 'Alice' },
-                    { name: 'Bob' }
-                ];
-                await this.users.insertMany(users);
-            }
-
-            const filesCount = await this.files.countDocuments();
-            if (filesCount === 0) {
-                const files = Array.from({ length: 30 }, (_, i) => ({ name: `file${i + 1}` }));
-                await this.files.insertMany(files);
-            }
         });
     }
 
